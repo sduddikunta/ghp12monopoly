b49be493f0c9f6cd958788a9590a11e071e9e843
Siddu Duddikunta <sduddikunta@gmail.com>
2012-07-03 09:39:57 -0400

    Added all the players.

--- src/com/ghp2012/monopoly/sim/GameInfo.java
+++ src/com/ghp2012/monopoly/sim/GameInfo.java
@@ -6,6 +6,13 @@ import com.ghp2012.monopoly.game.Player;
 public class GameInfo {
 	public static void fillGame(GameBoard g) {
 		g.addPlayer(new Player("Player 1", 1500));
-		//Finish adding players
+		g.addPlayer(new Player("Player 2", 1500));
+		g.addPlayer(new Player("Player 3", 1500));
+		g.addPlayer(new Player("Player 4", 1500));
+		g.addPlayer(new Player("Player 5", 1500));
+		g.addPlayer(new Player("Player 6", 1500));
+		g.addPlayer(new Player("Player 7", 1500));
+		g.addPlayer(new Player("Player 8", 1500));
+		// Add sets and properties, chance cards, community chest cards
 	}
 }
