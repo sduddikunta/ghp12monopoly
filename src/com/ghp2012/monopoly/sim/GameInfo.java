763223529f8c867e7fe4165169d968dc00481c24
Siddu Duddikunta <sduddikunta@gmail.com>
2012-07-03 09:35:13 -0400

    Added the GameInfo class.

---
+++ src/com/ghp2012/monopoly/sim/GameInfo.java
@@ -0,0 +1,11 @@
+package com.ghp2012.monopoly.sim;
+
+import com.ghp2012.monopoly.game.GameBoard;
+import com.ghp2012.monopoly.game.Player;
+
+public class GameInfo {
+	public static void fillGame(GameBoard g) {
+		g.addPlayer(new Player("Player 1", 1500));
+		//Finish adding players
+	}
+}
