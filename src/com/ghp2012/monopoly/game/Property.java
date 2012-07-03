d0a80cf19679c1481f0818003b92a5f367f0d24f
Siddu Duddikunta <sduddikunta@gmail.com>
2012-07-03 10:21:07 -0400

    Added rents.

--- src/com/ghp2012/monopoly/game/Property.java
+++ src/com/ghp2012/monopoly/game/Property.java
@@ -2,15 +2,23 @@ package com.ghp2012.monopoly.game;
 
 public enum Property {
 	
-	ST_JAMES_PLACE("St. James Place");
+	ST_JAMES_PLACE("St. James Place", 0, 0, 0, 0, 0, 0);
 	
-	private final String name;
+	public final String name;
+	public final int base;
+	public final int one;
+	public final int two;
+	public final int three;
+	public final int four;
+	public final int hotel;
 
-	private Property(String name) {
+	private Property(String name, int baserent, int rent1, int rent2, int rent3, int rent4, int renthotel) {
 		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
+		base = baserent;
+		one = rent1;
+		two = rent2;
+		three = rent3;
+		four = rent4;
+		hotel = renthotel;
 	}
 }
